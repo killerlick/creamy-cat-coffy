27db76eb525331cd95c7e63dc4eaddcccfae0a42
malick <chimpanfeu@gmail.com>
2026-05-16 13:03:31 -0400

    avancer dans le projet

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -1,14 +1,16 @@
-
 class_name Cat
 extends Node2D
 
 
 var drink_wanted : DrinkData
 
+var mouse_inside := false
+
 @onready var cat_sprite : Sprite2D = $"Cat sprite"
 @onready var bar_attente : ProgressBar = $Bar_attente
 @onready var order_bubble : Node2D =$Order_bubble
-@onready var order_text : Label = $Order_text
+@onready var order_text : Label = $Order_bubble/Order_text
+@onready var area : Area2D = $Area2D
 
  
 
@@ -19,20 +21,29 @@ func _ready() -> void:
 	await generate_drink()
 	print(drink_wanted.to_string())
 	set_order_text(drink_wanted.get_all_ingredients())
-	
 
+func receive_drink(drink : DrinkData) -> void:
+	var good_command = drink.is_equal(drink_wanted)
+	if good_command == true:
+		print("bonne command")
+	else:
+		print("mauvaise commande")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func set_order_text(orders:Array[String]):
 	var text = ""
 	for ingredient in orders:
 		text+= "-"+ingredient+"\n"
 	order_text.text = text
-	
 
 
 func generate_drink() -> void :
 	drink_wanted.random_drink()
+
+
+func _on_area_2d_mouse_entered() -> void:
+	mouse_inside = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	mouse_inside = false
--- scripts/drink_scene.gd
+++ scripts/drink_scene.gd
@@ -1,17 +1,49 @@
 extends Node2D
+class_name Drink
 
+var dragging : bool = false
+var original_position : Vector2
 var drink_data : DrinkData
+var is_selected : bool = false
 
 @onready var addons_sprite_container : Node2D = $addons
 @onready var liquid_sprite : Sprite2D = $liquid
 @onready var glass_sprite : Sprite2D = $glasses
 @onready var powder_sprite : Sprite2D = $powder
 @onready var topping_sprite_container : Node2D = $topping
+@onready var area :Area2D = $Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawning_topping()
-	spawning_addon()
+	if drink_data!=null:
+		spawning_topping()
+		spawning_addon()
+
+func _process(delta: float) -> void:
+	if dragging:
+		global_position = get_global_mouse_position()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed and is_selected ==true:
+					dragging=true
+					original_position = global_position
+			else:
+				if dragging:
+					dragging = false
+					on_drop()
+
+func on_drop():
+	var overlappings = area.get_overlapping_areas()
+	for obj in overlappings:
+		print(obj)
+		var cat = obj.get_parent()
+		if cat is Cat:
+			print("is a cat")
+			cat.receive_drink(drink_data)
+			return
+	global_position = original_position
 
 func spawning_addon() -> void:
 	for child in addons_sprite_container.get_children():
@@ -21,18 +53,18 @@ func spawning_addon() -> void:
 		sprite.texture = addon.element_sprite[0]
 		addons_sprite_container.add_child(sprite)
 
-
 func spawning_topping() -> void:
 	for child in topping_sprite_container.get_children():
 		child.queue_free()
-	for topping in drink_data.topping:
+	for topping in drink_data.toppings:
 		var sprite = Sprite2D.new()
 		sprite.texture = topping.element_sprite[0]
 		topping_sprite_container.add_child(sprite)
 
 
-func change_water_color(flavor :String) -> void:
-	if drink_data.liquid.element_name == "water":
-		#un truc pour changer la couler de l'eau
-		pass
-	pass
+func _on_area_2d_mouse_entered() -> void:
+	is_selected = true # Replace with function body.
+
+
+func _on_area_2d_mouse_exited() -> void:
+	is_selected = false # Replace with function body.
